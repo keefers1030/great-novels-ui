bcf9cce9cefc719b29de0e72a15f6ca92a0d71e2
Keith March Mistler <kmistler@gmail.com>
2021-06-13 13:30:25 -0400

    added component js file

---
+++ src/components/greatnovels.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+const greatNovels = props => {
+
+  return (
+    <div className="App">
+      <h1>Great Novels</h1>
+      <input type="text" name="search" onChange={(event) => props.setSearchTerm(event.target.value)}/>
+      {props.greatNovelsAsProps.map(greatNovels => {
+        return (
+          <>
+            <div>{greatNovels.title}</div>
+          </>
+        )
+      })}
+    </div>
+  )
+}
+
+export default greatNovels
