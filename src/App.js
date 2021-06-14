429c3445e80b35e621644dd4919641988530d7f4
Keith March Mistler <kmistler@gmail.com>
2021-06-14 12:29:31 -0400

    added useEffect language

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import greatNovels from './components/greatnovels'
-import {pulledData, filtered} from './utils/greatnovels'
+import { pulledData, filtered } from './utils/greatnovels'
 
 export default () => {
   const [greatNovelsData, setNovelsData] = useState([])
   const [filteredNovels, setFilteredNovels] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
+
   useEffect(() => {
     async function pullData() {
-      const data
+      const data = await pulledData()
+
+      setNovelsData(data)
+      setFilteredNovels(data)
     }
-  })
+    pullData()
+  }, [])
 }
